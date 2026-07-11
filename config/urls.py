85d372f7923c2ca25c38b715214061262bec0b41
Anusree <anusreeanu1357@gmail.com>
2026-07-11 22:57:42 +0530

    Temporary reset admin url

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,20 @@ from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth import get_user_model
+from django.http import HttpResponse
+
+def reset_password(request):
+    User = get_user_model()
+    u, created = User.objects.get_or_create(username='admin', defaults={'email': 'admin@example.com'})
+    u.set_password('Admin1234!')
+    u.is_superuser = True
+    u.is_staff = True
+    u.save()
+    return HttpResponse("Password has been reset! You can now log into the admin panel with username: admin and password: Admin1234!")
+
 urlpatterns = [
+    path('reset-admin-password-123/', reset_password),
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('accounts/', include('accounts.urls')),
