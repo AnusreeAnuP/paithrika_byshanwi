014fbd7970e65175172cce9bd72e507349fa05a4
Anusree <anusreeanu1357@gmail.com>
2026-07-14 12:51:07 +0530

    Add multiple images per product using ProductImage model

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product
+from .models import Category, Product, ProductImage
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -7,9 +7,14 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'slug', 'brand')
     list_filter = ('brand',)
 
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'price', 'stock', 'is_available', 'is_new_arrival', 'is_best_seller', 'has_offer')
     list_filter = ('category', 'is_available', 'is_new_arrival', 'is_best_seller', 'has_offer')
     prepopulated_fields = {'slug': ('name',)}
     search_fields = ('name', 'description')
+    inlines = [ProductImageInline]
